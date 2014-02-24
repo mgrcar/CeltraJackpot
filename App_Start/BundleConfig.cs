160a6ae6e35ddd6ec339de4306f68e4944f32f0a
mgrcar <miha@sowalabs.com>
2014-02-24 10:20:45 +0100

    Empty MVC project

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace EmptyMvcProject
+{
+    public class BundleConfig 
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear(); // allow adding minified js files
+            BundleTable.EnableOptimizations = true;
+            // Styles
+            // ...
+            // Scripts
+            // ...
+        }
+    }
+}
