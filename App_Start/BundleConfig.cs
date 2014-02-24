90c152966280f0eb5f64bd2154ce8c48507f7c71
mgrcar <miha@sowalabs.com>
2014-02-24 10:45:18 +0100

    Included Bootstrap (3.1.1) and jQuery (2.1.0)

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,9 +9,16 @@ namespace EmptyMvcProject
             bundles.IgnoreList.Clear(); // allow adding minified js files
             BundleTable.EnableOptimizations = true;
             // Styles
-            // ...
+            bundles.Add(new StyleBundle("~/css/bootstrap").Include(
+                "~/css/bootstrap.css"
+                ));
             // Scripts
-            // ...
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
+                "~/js/jquery-2.1.0.min.js"
+                ));
+            bundles.Add(new ScriptBundle("~/js/bootstrap").Include(
+                "~/js/bootstrap.js"
+                ));
         }
     }
 }
