cf1fe244922fbd62c15084fe61dd313635ffc7aa
mgrcar <miha.grcar@celtra.com>
2014-11-29 21:51:22 +0100

    Updated single-submission eval script

--- Submissions/eval-one.ps1
+++ Submissions/eval-one.ps1
@@ -1,7 +1,11 @@
-cd $args[0]
+$c = $args[0]
+cd $c
 
 for ($i = 1; $i -le 100; $i++) {
 	for ($e = 1; $e -le 10; $e++) {
-		.\exec.ps1 $i $e
+		if (!(Test-Path "..\Results\$c.$i.$e.done")) {
+			Remove-Item "..\Results\$c.$i.$e.*"
+			.\exec.ps1 $i $e
+		}
 	}
 }
