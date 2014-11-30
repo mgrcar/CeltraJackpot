f815c29729b64c5889199ea30a74a078b71fbfe9
mgrcar <miha.grcar@celtra.com>
2014-11-30 17:13:35 +0100

    Added exec script for RL

---
+++ Submissions/rl/exec.ps1
@@ -0,0 +1,4 @@
+$i = $args[0]
+$e = $args[1]
+echo "C:\Python34\python.exe .\Jackpot\jackpot.py http://localhost/CeltraJackpot/rl.$i.$e/$e"
+C:\Python34\python.exe .\Jackpot\jackpot.py http://localhost/CeltraJackpot/rl.$i.$e/$e
