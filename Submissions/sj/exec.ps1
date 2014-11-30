5bf8dbdf6e176a02fe266102d9069ca9b5def55c
mgrcar <miha.grcar@celtra.com>
2014-11-30 17:31:26 +0100

    Added exec script for SJ

---
+++ Submissions/sj/exec.ps1
@@ -0,0 +1,4 @@
+$i = $args[0]
+$e = $args[1]
+echo "python .\celtra-izziv\celtra.py http://localhost/CeltraJackpot/sj.$i.$e/$e"
+python .\celtra-izziv\celtra.py http://localhost/CeltraJackpot/sj.$i.$e/$e
