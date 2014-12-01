7f9e1a8b28738060f8c58681283e887cd8039673
mgrcar <miha.grcar@celtra.com>
2014-12-01 16:37:24 +0100

    Added exec script for TC

---
+++ Submissions/tc/exec.ps1
@@ -0,0 +1,4 @@
+$i = $args[0]
+$e = $args[1]
+echo "python.exe .\avtomati\avtomati.py http://localhost/CeltraJackpot/tc.$i.$e/$e"
+python.exe .\avtomati\avtomati.py http://localhost/CeltraJackpot/tc.$i.$e/$e
