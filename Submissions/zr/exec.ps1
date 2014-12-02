d38734655724bcf5ccca9fdd50c665f04b22f415
mgrcar <miha.grcar@celtra.com>
2014-12-02 11:01:41 +0100

    Added exec script for ZR

---
+++ Submissions/zr/exec.ps1
@@ -0,0 +1,4 @@
+$i = $args[0]
+$e = $args[1]
+echo "C:\Anaconda3\python.exe .\celtra\source\bandit.py http://localhost/CeltraJackpot/zr.$i.$e/$e"
+C:\Anaconda3\python.exe .\celtra\source\bandit.py http://localhost/CeltraJackpot/zr.$i.$e/$e
