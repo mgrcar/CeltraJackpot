211b481ebfee3b0012df5aeeef201194072999a6
mgrcar <miha.grcar@celtra.com>
2014-12-07 13:15:22 +0100

    Fixed exec script (RL)

--- Submissions/rl/exec.ps1
+++ Submissions/rl/exec.ps1
@@ -1,4 +1,4 @@
 $i = $args[0]
 $e = $args[1]
-echo "C:\Python34\python.exe .\Jackpot\jackpot.py http://localhost/CeltraJackpot/rl.$i.$e/$e"
-C:\Python34\python.exe .\Jackpot\jackpot.py http://localhost/CeltraJackpot/rl.$i.$e/$e
+echo "C:\Anaconda3\python.exe .\Jackpot\jackpot.py http://localhost/CeltraJackpot/rl.$i.$e/$e"
+C:\Anaconda3\python.exe .\Jackpot\jackpot.py http://localhost/CeltraJackpot/rl.$i.$e/$e
